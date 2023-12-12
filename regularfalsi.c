ab8c9af1ca1c8069e42f7e8c164dda4856f1eb76
Hritik Raj <118843441+hritraj@users.noreply.github.com>
2023-12-13 00:43:01 +0530

    Create regularfalsi.c

---
+++ regularfalsi.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<math.h>
+#define e 0.00001
+#define f(x) (x*x*x-2*x-5)
+int main()
+{
+    float a,b,c;
+    int i=1;
+    printf("enter the values of a n b");
+    scanf("%f %f",&a,&b);
+    if(f(a)*f(b)<=0)
+    {
+        do
+        {
+            c=(a*f(b)-b*f(a))/(f(b)-f(a));
+            printf("i=%d |a=%.4f |b=%.4f |c=%.4f |f(a)=%.4f |f(b)=%.4f |f(c)=%.4f", i,a,b,c,f(a),f(b),f(c));
+            printf("\n");
+            if(f(a)*f(c)<0)
+            {
+                a=a;
+                b=c;
+            }
+            else
+            {
+                a=c;
+                b=b;
+            }
+            i++;
+
+        }
+        while(fabs(f(c))>e);
+        printf("\n");
+        printf("root is %0.4f",c);
+    }
+    else
+    {
+        printf("not exist");
+
+    }
+    return 0;
+
+}
